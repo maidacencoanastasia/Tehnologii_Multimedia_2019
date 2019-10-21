fc1e37d23e41a9041a5e54f7a2a8a35cb3181465
Maidacenco Anastasia <42982928+maidacencoanastasia@users.noreply.github.com>
2019-10-21 10:12:31 +0300

    Add files via upload

--- p4/main.js
+++ p4/main.js
@@ -1,4 +1,101 @@
+var student = /** @class */ (function () {
+    function student(ob) {
+        this.NP = ob.NP;
+        this.note = ob.note;
+        this.gr = ob.gr;
+    }
+    //---------------------------
+    student.prototype.getHtmlStudent = function () {
+        return "\n         <tr> \n          <td>" + this.NP + " </td>\n          <td>" + this.notaMedie().toFixed(2) + " </td>\n          <td>" + this.gr + " </td>          \n         </tr>\n       ";
+    };
+    //---------------------------
+    student.prototype.restanta = function () {
+        for (var _i = 0, _a = this.note; _i < _a.length; _i++) {
+            var nota = _a[_i];
+            if (nota < 5) {
+                return true;
+            }
+        }
+        return false;
+    };
+    //---------------------------
+    student.prototype.notaMedie = function () {
+        var suma = 0;
+        for (var _i = 0, _a = this.note; _i < _a.length; _i++) {
+            var nota = _a[_i];
+            suma += nota;
+        }
+        var n = this.note.length;
+        return (n > 0) ? suma / n : 0;
+    };
+    return student;
+}());
+/**
+ * JSON de studenti
+ *
+ */
+var JSON_students = "\n{\n    \"students\":[\n        {\n            \"NP\":\"Niconor ION\",\n            \"note\":[5,7,9],\n            \"gr\":\"is21z\"\n        },\n        {\n            \"NP\":\"Mihai Constantin\",\n            \"note\":[10,9,9],\n            \"gr\":\"mi21z\"\n        }\n    ]\n}";
 document.addEventListener('DOMContentLoaded', function () {
-    alert();
-    console.log('este');
+    var students = [];
+    var JS_students = JSON.parse(JSON_students);
+    var info = JS_students.students;
+    for (var _i = 0, info_1 = info; _i < info_1.length; _i++) {
+        var item = info_1[_i];
+        students.push(new student(item));
+    }
+    // console.log(students);
+    var p0 = document.querySelector("#all");
+    var p1 = document.querySelector("#is21z");
+    var p2 = document.querySelector("#mi21z");
+    _RENDER(0);
+    p0.addEventListener('click', function (e) {
+        _RENDER(0);
+    });
+    p1.addEventListener('click', function (e) {
+        _RENDER(1);
+    });
+    p2.addEventListener('click', function (e) {
+        _RENDER(2);
+    });
+    function _RENDER(p) {
+        p0.classList.remove("selected");
+        p1.classList.remove("selected");
+        p2.classList.remove("selected");
+        if (p == 0) {
+            p0.classList.add("selected");
+            var temp = '<table>';
+            for (var _i = 0, students_1 = students; _i < students_1.length; _i++) {
+                var item = students_1[_i];
+                temp += item.getHtmlStudent();
+            }
+            temp += '</table>';
+            var ob = document.querySelector('#lista');
+            ob.innerHTML = temp;
+        }
+        ;
+        if (p == 1) {
+            p1.classList.add("selected");
+            var temp = '<table>';
+            var filtru = students.filter(function (c) { return c.gr == 'is21z'; });
+            for (var _a = 0, filtru_1 = filtru; _a < filtru_1.length; _a++) {
+                var item = filtru_1[_a];
+                temp += item.getHtmlStudent();
+            }
+            temp += '</table>';
+            var ob = document.querySelector('#lista');
+            ob.innerHTML = temp;
+        }
+        if (p == 2) {
+            p2.classList.add("selected");
+            var temp = '<table>';
+            var filtru = students.filter(function (c) { return c.gr == 'mi21z'; });
+            for (var _b = 0, filtru_2 = filtru; _b < filtru_2.length; _b++) {
+                var item = filtru_2[_b];
+                temp += item.getHtmlStudent();
+            }
+            temp += '</table>';
+            var ob = document.querySelector('#lista');
+            ob.innerHTML = temp;
+        }
+    }
 });
--- p4/main.ts
+++ p4/main.ts
@@ -1,4 +1,152 @@
+class student{
+    
+    NP:string;    
+    note:Array<number>;
+    gr:string;
+
+   constructor(ob:any){
+        
+    this.NP = ob.NP;
+    this.note = ob.note;
+    this.gr = ob.gr;
+
+   }
+
+ //---------------------------
+   getHtmlStudent():string{
+       return `
+         <tr> 
+          <td>${this.NP} </td>
+          <td>${this.notaMedie().toFixed(2)} </td>
+          <td>${this.gr} </td>          
+         </tr>
+       `;
+   }
+//---------------------------
+   restanta():boolean{
+
+     for (let nota of this.note){
+         if (nota <5) { return true}
+     }
+     return false;
+   }
+//---------------------------
+   notaMedie(){
+       let suma:number=0;              
+       for(let nota of this.note){
+         suma+=nota;   
+       }
+       let n = this.note.length;
+       return (n>0)?suma/n:0;
+   }
+  //--------------------------- 
+
+}
+
+/**
+ * JSON de studenti
+ * 
+ */
+
+var JSON_students = `
+{
+    "students":[
+        {
+            "NP":"Niconor ION",
+            "note":[5,7,9],
+            "gr":"is21z"
+        },
+        {
+            "NP":"Mihai Constantin",
+            "note":[10,9,9],
+            "gr":"mi21z"
+        }
+    ]
+}`;
+
 document.addEventListener('DOMContentLoaded', function(){
-    alert();
-    console.log('este');
+    
+    let students: Array<student>=[];     
+
+    let JS_students = JSON.parse(JSON_students);
+    let info:any = JS_students.students;
+
+    for (let item of info){
+        students.push(new student(item));        
+    }
+
+   
+
+    // console.log(students);
+    let p0 = document.querySelector("#all");
+    let p1 = document.querySelector("#is21z");
+    let p2 = document.querySelector("#mi21z");
+
+    _RENDER(0);
+
+    p0.addEventListener('click', function(e){
+        _RENDER(0);
+    })
+    
+    p1.addEventListener('click', function(e){
+        _RENDER(1);
+    })
+
+    p2.addEventListener('click', function(e){
+        _RENDER(2);
+    })
+
+
+    function _RENDER(p:number){
+
+
+        p0.classList.remove("selected");
+        p1.classList.remove("selected");
+        p2.classList.remove("selected");
+
+
+
+
+        if (p==0){
+
+            p0.classList.add("selected");
+            let temp = '<table>';
+            for(let item of students){
+                temp+=item.getHtmlStudent();        
+            }
+            temp += '</table>';
+            let ob = document.querySelector('#lista');
+            ob.innerHTML = temp; 
+        };
+
+        if (p==1){
+            p1.classList.add("selected");
+            let temp = '<table>';
+            let filtru = students.filter((c)=>c.gr=='is21z');
+            for(let item of filtru ){
+                temp+=item.getHtmlStudent();        
+            }
+            temp += '</table>';
+            let ob = document.querySelector('#lista');
+            ob.innerHTML = temp; 
+        }
+
+        if (p==2){
+            p2.classList.add("selected");
+            let temp = '<table>';
+            let filtru = students.filter((c)=>c.gr=='mi21z');
+            for(let item of filtru ){
+                temp+=item.getHtmlStudent();        
+            }
+            temp += '</table>';
+            let ob = document.querySelector('#lista');
+            ob.innerHTML = temp; 
+        }
+
+
+
+    } 
+
+
+
 })
